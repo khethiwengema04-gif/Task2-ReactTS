486fe44dc76eb09a14f72a333b36fe13476e4a0c
khethiwe.ngema04 <khethiwe.ngema04@gmail.com>
2026-07-21 14:51:32 +0200

    Created navbar component

---
+++ Task2-ReactTS/src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css'
+import Navbar from './Components/Navbar/Navbar'
+import {Text} from './Components/Text/Text'
+
+
+
+function App() {
+
+  return (
+    <div id='app-container'>
+      <div id='scrollable'>
+        <Navbar/>
+      
+      
+       </div>  
+      
+    </div>
+  )
+}
+
+export default App
---
+++ Task2-ReactTS/src/Components/Search/Search.tsx
@@ -0,0 +1,16 @@
+// import React from 'react'
+// import { ContentContainer } from '../ContentContainer'
+// import style from './Search.module.css'
+
+// export const Search = () => {
+//   return (
+//     <div className={style['search-container']}>
+//         <ContentContainer className={style['searchbar-container']}>
+//             <Search/>
+         
+         
+//          </ContentContainer>
+    
+//     </div>
+//   )
+// }
